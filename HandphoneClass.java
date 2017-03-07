c427a5900e21c95864df09b1d35e147b7176b69a
wihardodo <wihardodo@gmail.com>
2017-03-07 23:15:38 +0700

    Add files via upload

---
+++ HandphoneClass.java
@@ -0,0 +1,15 @@
+packpage handphone;
+		public class Handphone {
+			String merk;
+			String tombol;
+			int tahunbuat;
+			
+				public void menyala(){
+					System.out.println("menyala");}   
+				public void menelfon(){
+					System.out.println("menelfon");}
+				public void chat(){
+					System.out.println("chat");}
+
+}
+
---
+++ PanggilClass.java
@@ -0,0 +1,8 @@
+package handphone;
+		public class ClassUtama {
+			public static void main(String[]args){
+				Handphone samsung = new Handphone();
+
+					samsung.merk="Samsung Galaxy S7";
+				samsung.menelfon();}
+}
